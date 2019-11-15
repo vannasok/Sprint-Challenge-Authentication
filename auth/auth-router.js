a9356eafb9973acf6567b60ce8ac289fa2e57081
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-15 09:23:01 -0800

    endpoints test

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,63 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('./user-model.js');
+const restricted = require('./authenticate-middleware.js');
+const secrets = require('../secrets');
+
 router.post('/register', (req, res) => {
-  // implement registration
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+	user.password = hash;
+
+	Users.register(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user.username);
+				res.status(200).json({
+					message: `Log in as > ${user.username}`,
+					token
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
+});
+
+function generateToken(username) {
+	const payload = {
+		username
+	};
+	const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+	const options = {
+		expiresIn: '1d'
+	};
+	return jwt.sign(payload, secret, options);
+}
+
+router.get('/users', restricted, (req, res) => {
+	Users.find()
+		.then(users => {
+			res.json({ loggedInUser: req.username, users });
+		})
+		.catch(err => res.send(err));
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+	const token = req.headers.authorization;
+
+	if (token) {
+		const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+
+		jwt.verify(token, secret, (err, decodedToken) => {
+			if (err) {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			} else {
+				req.decodedJwt = decodedToken;
+				next();
+			}
+		});
+	} else {
+		res.status(400).json({ message: 'No Credentials Provided' });
+	}
 };
